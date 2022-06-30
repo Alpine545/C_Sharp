99afdd748a37a1dec1ac658b21e03c5e5b2b0802
Alpine545 <Alpine545@mail.ru>
2022-06-30 21:05:13 +0300

    1 Exercise

--- 1_Lesson/1_1/Program.cs
+++ 1_Lesson/1_1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, People!");
+﻿//string num_str = Console.ReadLine();
+//int num = int.Parse(num_str);
+//int num = int.Parse(Console.ReadLine());
+//Console.WriteLine($"{num}^2: {num * num}");
+
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+
+if (num2 == num1 * num1)
+{
+    Console.WriteLine("Yes");
+}
+
+else
+{
+    Console.WriteLine("No");
+}
