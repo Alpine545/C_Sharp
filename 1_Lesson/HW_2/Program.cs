ba04f08d2cbe666631937117eeae7eccf5034a27
Alpine545 <Alpine545@mail.ru>
2022-07-02 16:05:41 +0300

    HW_2

---
+++ 1_Lesson/HW_2/Program.cs
@@ -0,0 +1,23 @@
+﻿int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+int z = int.Parse(Console.ReadLine());
+int index = 0;
+int max = 0;
+
+if (x>y)
+{
+    max = x;
+}
+else
+{
+    max = y;
+}
+
+if (max > z)
+{
+    Console.WriteLine(max);
+}
+else
+{
+    Console.WriteLine(z);
+}
