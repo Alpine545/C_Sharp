01870b7a3c4f36375d3bc1aad0fb756a36ba530b
Alpine545 <Alpine545@mail.ru>
2022-07-02 15:22:12 +0300

    5 Exercise

--- 1_Lesson/1_4/Program.cs
+++ 1_Lesson/1_4/Program.cs
@@ -1,4 +1,8 @@
 ﻿int N = int.Parse(Console.ReadLine());
+if (N<=0)
+{
+    N = N*(-1);
+}
 int M = -N;
 
 while (M<N+1)
---
+++ 1_Lesson/1_5/Program.cs
@@ -0,0 +1,9 @@
+﻿int N = int.Parse(Console.ReadLine());
+if (N<=0)
+{
+    Console.WriteLine(N*(-1));
+}
+else
+{
+    Console.WriteLine(N);
+}
